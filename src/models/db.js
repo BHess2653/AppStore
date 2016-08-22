2c868b1e877f0e77da72d46e90535cd2dbd83c8a
BHess2653 <GM_Dahaka@BHMac.local>
2016-08-22 04:44:10 -0400

    updated db.js and package.json

--- src/models/db.js
+++ src/models/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const utool = require('fs-uTool');
 
-if (!process.env.DB_HOST) require('dotenv').config();
+if (!process.env.DB_HOST) require('dotenv').config({ silent: true });
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // Sets up database
